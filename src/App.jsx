8289a4252469cc0ce00accbe4b9d64e5394de0af
fckillernaver <fckiller@naver.com>
2024-12-13 09:27:10 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RandomBox from "./RandomBox";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <RandomBox />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/RandomBox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RandomBox = () => {
+  return (
+    <div>
+      <p>윤석열 내란수괴 탄핵 ㄱㄱ</p>
+      <p>김건희도 같이 ㄱㄱ</p>
+    </div>
+  );
+};
+
+export default RandomBox;
